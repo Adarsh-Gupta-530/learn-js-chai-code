5098146e9c0c66690862d7ff4d7d1fa04b926179
Adarsh-Gupta-530 <adarshgupta4530@gmail.com>
2024-08-30 16:29:07 +0000

    variable var let const

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+const accountId = 1445532
+let accountEmail = "abc@gmail.com"
+var accountPass = "1234500"
+accountCity = "jabalpur"
+
+// accountId = 2//not allowed
+console.log(accountId);
+console.table([accountEmail,accountId,accountPass,accountCity]);
+
